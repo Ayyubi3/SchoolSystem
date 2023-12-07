f51c8359a27166633cf9ccc7fd60236af0325124
Eyup3 <eyup.ozturk@outlook.de>
2023-12-07 04:36:57 +0100

    Install and Setup ejs engine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+require("dotenv").config()
+
 const express = require('express')
 const app = express()
 
-require("dotenv").config()
 const port = process.env.SERVER_PORT
 
+
+app.set("view-engine", "ejs")
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render(require("path").join("..", "public", "index", "index.ejs"), {loggedIn: false, user: "Max", alertMessage: null})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Express Server gestartet -> PORT: ${port}`)
 })
