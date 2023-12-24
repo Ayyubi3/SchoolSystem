2093e057b79d532feb65ce9abab79504b6d91e3b
Eyup3 <eyup.ozturk@outlook.de>
2023-12-24 07:45:53 +0100

    socketio rooms work

--- src/libs/SocketUtils.js
+++ src/libs/SocketUtils.js
@@ -1,21 +1,49 @@
 const io = require('socket.io')(process.env.SOCKETPORT, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["my-custom-header"],
-      credentials: true
-    }
-  })
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+})
 
 const users = {}
 
 io.on('connection', socket => {
-  console.log(socket)
 
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
   })
+
+
+  socket.on("join-room", room => {
+    socket.join(room)
+  })
+
+
+  socket.on('send-chat-message', ({ name, room, message }) => {
+
+    
+    socket.to(room).emit("chat-message",
+      {
+        message,
+        name
+      }
+    )
+
+
+  })
+
+
+
+
+
+
+
+  /*
+
+
 
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
@@ -24,6 +52,6 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
-  })
+  })*/
 
 })
