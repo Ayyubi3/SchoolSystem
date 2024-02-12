78f6f7314076b7b05f8dfb28218e9637dbbb5001
Eyup3 <eyup.ozturk@outlook.de>
2024-02-12 01:12:18 +0100

    view and route directory and general route handling

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(require("./route/router"))
+
+app.use("/", express.static(require("path").join(__dirname, "view")))
+
 const port = process.env.EXPRESS_SERVER_PORT
 app.listen(port, () => {
+  console.log(`Listening to ${port}`)
 })
